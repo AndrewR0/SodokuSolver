7b81aa21694659fc49255c6d2682fb0a36854993
Andrew <andrewrednikov@outlook.com>
2021-12-09 16:18:24 -0500

    Changed struture of code to be in a class, separated board into different file, implemented validation across rows, cols, and box

--- Solver.py
+++ Solver.py
@@ -1,28 +1,60 @@
-board = [[0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,1],
-         [0,0,0,5,0,6,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,2,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0]]
+import numpy as np
+from board import board
+import math
 
-def printBoard(board):
-    output = ""
-    rows = len(board)
-    cols = len(board[0])
-    for i in range(rows):
-        for j in range(cols):
-            if (j+1) % 3 == 0 and j != 0:
-                output += str(board[i][j]) + "|"
-            else:
-                output += str(board[i][j]) + " "
-        output += "\n"
+class Solve:
+    def __init__(self, board):
+        #self.board = board
+        self.rows = len(board)
+        self.cols = len(board[0])
+        self.factor = int(self.rows ** 0.5) #
+
+    def validate(self, board, pos, num):
         
-        if (i+1) % 3 == 0 and i != 0 and i != rows-1:
-            output += "- - - - - - - - -\n"
-    return output
-                
+        T = np.array(board)
+
+        tempRow = T[pos[0]] #extract row from matrix
+        tempCol = T[:,pos[1]] #extract col from matrix
+
+        row = [i for i in tempRow if i != 0]
+        col = [j for j in tempCol if j != 0]
+        #print(row, col)
+
+        if len(set(row)) != len(row) or len(set(col)) != len(col):
+            return False
+        
+        #box (4,1)
+        r = int(pos[0] / self.factor)
+        c = int(pos[1] / self.factor)
+
+        #print(r,c)
+
+        for x in range(self.factor):
+            for y in range(self.factor):
+                val = board[x + r*3][y + c*3]
+                if val != 0 and val == num:
+                    return False
+                else:
+                    continue
+
+    def solve(self, board):
+        pass
+    
+    def printBoard(self, board): #done for a 9x9, maybe change later for make for nxn (check if the n's are perfect squares)
+        output = ""
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if (j+1) % 3 == 0 and j != 0 and j != self.cols-1:
+                    output += str(board[i][j]) + "|"
+                else:
+                    output += str(board[i][j]) + " "
+            output += "\n"
+            
+            if (i+1) % 3 == 0 and i != 0 and i != self.rows-1:
+                output += "- - - - - - - - -\n"
+        return output
+
 if __name__ == '__main__':
-    print(printBoard(board))
+    x = Solve(board)
+    print(x.printBoard(board))
+    print(x.validate(board, (8,8), 9))
